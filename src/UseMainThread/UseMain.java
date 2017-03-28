5c433bed9853a34c4adc2528cdd06d2fad896099
ZhenyaPcW <zTiff0@gmail.com>
2017-03-28 15:08:39 +0300

    123

--- src/UseMainThread/UseMain.java
+++ src/UseMainThread/UseMain.java
@@ -1,5 +1,29 @@
 package UseMainThread;
 
 public class UseMain {
+    public static void main(String[] args) {
+        Thread thrd;
 
+        // Clone main thread
+        thrd = Thread.currentThread();
+
+        // Show name of main thread
+        System.out.println("Name of main thread: " + thrd.getName());
+
+        // Show prority of main thread
+        System.out.println("Priority " + thrd.getPriority());
+
+        System.out.println();
+
+        //Set name and priority of main thread
+        System.out.println("Set name and priority\n");
+
+        thrd.setName("Thread #1");
+        thrd.setPriority(Thread.NORM_PRIORITY + 3);
+
+        System.out.println("New name of thread " + thrd.getName());
+
+        System.out.println("New priority of thread " + thrd.getPriority());
+
+    }
 }
